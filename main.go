7e1bac69aec1893515c00af98de8e7237a85a22d
David Quintero <davidquintero@Davids-MBP.lan>
2022-01-23 13:17:11 -0500

    server boilerplate

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+
+		d, _ := ioutil.ReadAll(r.Body)
+		log.Printf("Data %s\n", d)
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
